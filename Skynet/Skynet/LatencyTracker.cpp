24f3ead2003602bad57209b6aa19aac5ae2458a2
Dmitry Kashitsyn <korvin@deeptown.org>
2012-06-11 22:00:47 +0700

    Adds zerg spells to the LatencyTracker

--- Skynet/Skynet/LatencyTracker.cpp
+++ Skynet/Skynet/LatencyTracker.cpp
@@ -5,22 +5,46 @@ const int StasisRadius = 96;
 
 void LatencyTrackerClass::update()
 {
-	const int time = BWAPI::Broodwar->getFrameCount();
-	for(std::map<Unit, std::pair<Position, int>>::iterator it = mStormedPositions.begin(); it != mStormedPositions.end();)
-	{
-		if(!it->first->exists() || (it->first->getOrder() != BWAPI::Orders::CastPsionicStorm && time > it->second.second))
-			mStormedPositions.erase(it++);
-		else
-			++it;
-	}
+        if (BWAPI::Broodwar->self()->getRace() == BWAPI::Races::Protoss)
+        {
+                const int time = BWAPI::Broodwar->getFrameCount();
+                for(std::map<Unit, std::pair<Position, int>>::iterator it = mStormedPositions.begin(); it != mStormedPositions.end();)
+                {
+                        if(!it->first->exists() || (it->first->getOrder() != BWAPI::Orders::CastPsionicStorm && time > it->second.second))
+                                mStormedPositions.erase(it++);
+                        else
+                                ++it;
+                }
 
-	for(std::map<Unit, std::pair<Position, int>>::iterator it = mStasisPositions.begin(); it != mStasisPositions.end();)
-	{
-		if(!it->first->exists() || (it->first->getOrder() != BWAPI::Orders::CastStasisField && time > it->second.second))
-			mStasisPositions.erase(it++);
-		else
-			++it;
-	}
+                for(std::map<Unit, std::pair<Position, int>>::iterator it = mStasisPositions.begin(); it != mStasisPositions.end();)
+                {
+                        if(!it->first->exists() || (it->first->getOrder() != BWAPI::Orders::CastStasisField && time > it->second.second))
+                                mStasisPositions.erase(it++);
+                        else
+                                ++it;
+                }
+        } else if (BWAPI::Broodwar->self()->getRace() == BWAPI::Races::Zerg)
+        {
+                for (std::map<Unit, Unit>::iterator iBroodling = mPendingBroodlings.begin(); iBroodling != mPendingBroodlings.end(); ++iBroodling)
+                {
+                        if (!iBroodling->first->exists() || (iBroodling->first->getOrder() != BWAPI::Orders::CastSpawnBroodlings))
+                        {
+                                mBroodlingVictims.erase(iBroodling->second);
+                                mPendingBroodlings.erase(iBroodling++);
+                        } else
+                                ++iBroodling;
+                }
+                
+                for (std::map<Unit, Unit>::iterator iParasite = mPendingParasites.begin(); iParasite != mPendingParasites.end(); ++iParasite)
+                {
+                        if (!iParasite->first->exists() || (iParasite->first->getOrder() != BWAPI::Orders::CastParasite))
+                        {
+                                mParasiteVictims.erase(iParasite->second);
+                                mPendingParasites.erase(iParasite++);
+                        } else
+                                ++iParasite;
+                }
+        }
 }
 
 void LatencyTrackerClass::placingStorm(Unit unit, Position pos)
@@ -60,3 +84,30 @@ bool LatencyTrackerClass::isStasisInRange(Unit unit)
 
 	return false;
 }
+
+void LatencyTrackerClass::placingEnsnare ( Unit who, Position where ) {
+        mPendingEnsnares[who] = std::make_pair(where, BWAPI::Broodwar->getFrameCount() + BWAPI::Broodwar->getRemainingLatencyFrames());
+}
+
+void LatencyTrackerClass::placingBroodlings ( Unit who, Unit target ) {
+        mPendingBroodlings[who] = target;
+        mBroodlingVictims.insert(target);
+}
+
+void LatencyTrackerClass::placingParasite ( Unit who, Unit target ) {
+        mPendingParasites[who] = target;
+        mParasiteVictims.insert(target);
+}
+
+bool LatencyTrackerClass::isSpawnBroodlingPending ( Unit victim ) {
+        return mBroodlingVictims.find(victim) != mBroodlingVictims.end();
+}
+
+bool LatencyTrackerClass::isParasitePending ( Unit victim ) {
+        return mParasiteVictims.find(victim) != mParasiteVictims.end();
+}
+
+bool LatencyTrackerClass::isEnsnarePending ( Position where ) {
+        return false; // TODO
+}
+
--- Skynet/Skynet/LatencyTracker.h
+++ Skynet/Skynet/LatencyTracker.h
@@ -14,13 +14,29 @@ public:
 
 	void placingStorm(Unit unit, Position pos);
 	void placingStasis(Unit unit, Position pos);
+        
+        void placingEnsnare(Unit who, Position where);
+        void placingBroodlings(Unit who, Unit target);
+        void placingParasite(Unit who, Unit target);
 
 	bool isStormInRange(Unit unit);
 	bool isStasisInRange(Unit unit);
+        
+        bool isSpawnBroodlingPending(Unit victim);
+        bool isParasitePending(Unit victim);
+        bool isEnsnarePending(Position where);
 
 private:
 	std::map<Unit, std::pair<Position, int>> mStormedPositions;
 	std::map<Unit, std::pair<Position, int>> mStasisPositions;
+        
+        std::map<Unit, std::pair<Position, int>> mPendingEnsnares; // TODO
+        
+        std::map<Unit, Unit> mPendingBroodlings;
+        std::set<Unit> mBroodlingVictims;
+        
+        std::map<Unit, Unit> mPendingParasites;
+        std::set<Unit> mParasiteVictims;
 };
 
 typedef Singleton<LatencyTrackerClass> LatencyTracker;
